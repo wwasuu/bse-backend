a289cf4c6fe283bda4081f01041523567a63ee33
wasu winitmontri <wasu.win@ascendcorp.com>
2020-08-18 11:55:45 +0700

    initial

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const socket = require("socket.io");
+const app = express();
+const port = 8080;
+
+const server = http.createServer(app);
+const io = socket.listen(server);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+io.emit("event", {
+  node: "A13",
+}); // This will emit the event to all connected sockets
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
