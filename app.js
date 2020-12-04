e1f9141da211d56da5a67544d43785be7325f34e
Wasu Winitmontri <khing9277@hotmail.com>
2020-12-04 15:21:05 +0700

    debug socket

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.post("/setting", async (req, res) => {
   try {
     const data = req.body
     const token = req.body.token
-    io.to(token).emit('setting', {
+    io.to('client:' + token).emit('setting', {
       token,
       temp: parseFloat(data.tempurature),
       light: parseInt(data.light || 0),
